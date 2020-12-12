ec9ece8aa507560808588ee14d9be54e194a2fe5
Matthew Rockwell <smugleafdev@gmail.com>
2020-12-12 14:08:45 -0700

    Add mostly working bullet shooting functionality

--- Assets/Scripts/FPSController.cs
+++ Assets/Scripts/FPSController.cs
@@ -9,6 +9,7 @@ public class FPSController : MonoBehaviour
     public float jumpSpeed = 8.0f;
     public float gravity = 20.0f;
     public Camera playerCamera;
+    public GameObject emitter;
     public float lookSpeed = 2.0f;
     public float lookXLimit = 45.0f;
 
@@ -66,6 +67,7 @@ public class FPSController : MonoBehaviour
             rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
             rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
             playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+            emitter.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
             transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
         }
     }
---
+++ Assets/Scripts/Shooting.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public GameObject bullet;
+    public float speed = 100f;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 forward = transform.TransformDirection(Vector3.forward);
+
+            GameObject instBullet = Instantiate(bullet, transform.position, Quaternion.identity) as GameObject;
+            Rigidbody instBulletRigidBody = instBullet.GetComponent<Rigidbody>();
+            instBulletRigidBody.AddForce(forward * speed);
+        }
+    }
+}
