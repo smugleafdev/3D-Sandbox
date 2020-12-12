401ef8ef8137a8abd5670680c33c739a4bf1c85a
Matthew Rockwell <smugleafdev@gmail.com>
2020-12-12 13:10:22 -0700

    Create enemy with health manager script

---
+++ Assets/Scripts/EnemyHealthManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealthManager : MonoBehaviour
+{
+    public int enemyMaxHealth;
+    private int enemyCurrentHealth;
+    private bool shouldDie = false;
+
+    void Start()
+    {
+        enemyCurrentHealth = enemyMaxHealth;
+    }
+
+    void Update()
+    {
+        if (enemyCurrentHealth <= 0)
+        {
+            shouldDie = true;
+        }
+
+        if (shouldDie)
+        {
+            HandleDeath();
+        }
+    }
+
+    public void DamageEnemy(int damage)
+    {
+        enemyCurrentHealth -= damage;
+    }
+
+    private void HandleDeath()
+    {
+        Destroy(gameObject);
+        // TODO: Restart spawner stuff probably go here
+    }
+
+    private void SetMaxHealth()
+    {
+        enemyCurrentHealth = enemyMaxHealth;
+    }
+
+}
