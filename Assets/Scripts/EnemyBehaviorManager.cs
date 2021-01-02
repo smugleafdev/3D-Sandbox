20f72e55196c5737e850fc2143e3ebfd345fd518
Matthew Rockwell <smugleafdev@gmail.com>
2021-01-02 13:59:21 -0700

    Add colored enemy hands, rotate enemy towards target

---
+++ Assets/Scripts/EnemyBehaviorManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class EnemyBehaviorManager : MonoBehaviour {
+
+    Transform target;
+    [SerializeField] float turnSpeed = 25f;
+
+    void Start() {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void Update() {
+        FaceTarget();
+    }
+
+    private void FaceTarget() {
+        // Debug.Log("Facing " + target);
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * turnSpeed);
+    }
+}
