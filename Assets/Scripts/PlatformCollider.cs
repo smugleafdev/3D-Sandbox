4e726e7a322299caad0f1cbe39feafd0603ffa02
Matthew Rockwell <smugleafdev@gmail.com>
2021-01-07 19:35:06 -0700

    Increase player bullet speed, add moving platform that player rides, increase player look angle from 45 to 90, turn stairs into prefab, turn on AutoSyncTransforms

---
+++ Assets/Scripts/PlatformCollider.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PlatformCollider : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.tag == "Player") {
+            transform.GetComponentInParent<PlatformMovement>().active = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (other.tag == "Player") {
+            transform.GetComponentInParent<PlatformMovement>().active = false;
+        }
+    }
+}
---
+++ Assets/Scripts/PlatformMovement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PlatformMovement : MonoBehaviour {
+
+    [SerializeField] GameObject playa;
+    [SerializeField] float speed = 2f;
+    public bool active;
+
+    private void Update() {
+        if (active) {
+            Vector3 move = new Vector3(speed * Time.deltaTime, 0, 0);
+            transform.Translate(move);
+            playa.transform.Translate(move, transform);
+        }
+    }
+}
